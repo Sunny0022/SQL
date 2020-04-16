daf88bfc94c0a7cf0e6e2a4a8f8fdbe5c6d5bce4
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-04-16 16:17:24 +0530

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,21 @@
+-- Create Table------------
+ 
+CREATE TABLE testing_table(name VARCHAR(30), contact_name VARCHAR(30), roll_no VARCHAR(30));
+ -- DELETE Column Name --------------
+ 
+ALTER TABLE testing_table DROP COLUMN name;
+ -- RENAME ---------
+ 
+ ALTER TABLE testing_table RENAME COLUMN contact_name to username;
+ -- Adding new field --------
+ 
+ALTER TABLE testing_table ADD first_name VARCHAR(30);
+ALTER TABLE testing_table ADD last_name VARCHAR(30);
+ 
+ -- Change Column type------------
+ 
+ALTER TABLE testing_table MODIFY roll_no INT(32);
+
+-- SHOW TABLE testing_table---------
+
+SELECT * FROM testing_table;
